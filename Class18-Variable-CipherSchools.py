148aad25e87ad45d66c298e460f797d73e4cfbe1
Suphal <suphalbochkar@gmail.com>
2022-11-28 23:08:39 +0530

    IP 3
    Variable, c1 summary, Concatination, input, int(),mare variables and inputs,string formating E1 , Sol

---
+++ Class18-Variable-CipherSchools.py
@@ -0,0 +1,36 @@
+num1= 2
+print(num1)
+num1 = 4
+print(num1)
+
+
+name="Suphal"
+print(name)
+name=123
+print(name)
+
+
+# 1num=4             #!Error
+
+# for variable name it can start with letter, _ , ... , , 
+# but not number (1,2,3,4),!,#
+
+# in middle also u cant use @,#,$,%
+
+
+
+_name=123
+
+u1sd="suphal"
+
+
+user_1_name="Suphal"                #!snake is writing   Python
+
+userOneName = "suphal"              #!camel is writing   JAVA
+
+
+
+
+
+
+
---
+++ Class20-Concatination-CipherSchools.py
@@ -0,0 +1,10 @@
+first_name="Suphal"
+last_name="Bochkar"
+full_name=first_name+" "+last_name
+print(full_name)
+
+# print (first_name+3)                # !Error
+print (first_name+"3")
+print (first_name+str(3))
+
+print(first_name *3)
