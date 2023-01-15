f7afbbd833ccd35a0deb8aab8a3fb220e5feaff9
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-16 00:35:43 +0530

    54

--- Class54-SumOf Num-CipherSchools.py
+++ Class54-SumOf Num-CipherSchools.py
@@ -1,14 +1,22 @@
-# sum 1 to 10
+# # # sum 1 to 10
 
-total=0
-i=1
+# # total=0
+# # i=1
 
-while i<=10:
-    total+=i
-    i+=1
-print(total)
+# # while i<=10:
+# #     total+=i
+# #     i+=1
+# # print(total)
 
-while i<=20:
-    total+=i
-    i+=1
-print(total)
+# # while i<=20:
+# #     total+=i
+# #     i+=1
+# # print(total)
+# # # 
+
+# num = [0,1,2,3,4,5]
+# print(num[-1:-3])
+
+
+l = [1,2,3,4]
+d = {1:'q',2:}
